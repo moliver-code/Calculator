b9b4d5322160c99e66d81dfbcee522af76020e45
Matt Oliver <87980329+moliver-code@users.noreply.github.com>
2022-05-28 00:07:20 +0100

    Add event listeners to numbers, operators.
    These modify global variables in the js file that will be used to calculate and output. Event listeners also update the screen div.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log('hello')
+let firstOperand  ;
+let secondOperand  ;
+let operator;
+let startingScreen; 
+
+var compute ={
+    '+': function(firstOperand, secondOperand) {return firstOperand + secondOperand},
+    '-': function(firstOperand, secondOperand) {return firstOperand - secondOperand},
+    '*': function(firstOperand, secondOperand) {return firstOperand * secondOperand},
+    '/': function(firstOperand, secondOperand) {return firstOperand / secondOperand},
+};
+
+// console.log(compute['/'](2,3));
+
+let screenText = document.getElementsByClassName('screen')[0]
+// console.log(screenText)
+screenText.textContent = startingScreen
+
+const numbersList = [...document.querySelectorAll('.number')]
+// console.log(numbersList)
+
+numbersList.forEach(element => {
+    element.addEventListener('click', e => {
+        if (!firstOperand) {
+            firstOperand = element.innerText;
+            screenText.textContent = firstOperand
+            return
+        } ;
+        if (!secondOperand) {
+            secondOperand = element.innerText;
+            screenText.textContent = secondOperand
+            return;
+        };
+    })
+});
+
+const operatorsList = [...document.querySelectorAll('.operator')]
+
+operatorsList.forEach(element => {
+    element.addEventListener('click', e => {
+        operator = element.getAttribute('action');
+        screenText.textContent = element.textContent;
+    })
+})
+
