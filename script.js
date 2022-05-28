1e69db411d001a438065b8e9d86bbf6599ad3b43
Matt Oliver <87980329+moliver-code@users.noreply.github.com>
2022-05-28 12:12:19 +0100

    Add Clear, delete functions, algo inprove
    Added clear and delete functions. Alogrithm modified to allow >1
    character on 2nd operand. Result is trimmed to 9 significant figures.
    Divide by zero not working.
    Chaining operators without hitting = button not working.

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-let firstOperand  ;
-let secondOperand  ;
+let firstOperand = '' ;
+let secondOperand = '' ;
 let operator;
-let startingScreen; 
+// let startingScreen; 
+let result;
+let focusOnSecondOperand;
+let currentResult = ''
 
 var compute ={
     '+': function(firstOperand, secondOperand) {return firstOperand + secondOperand},
     '-': function(firstOperand, secondOperand) {return firstOperand - secondOperand},
     '*': function(firstOperand, secondOperand) {return firstOperand * secondOperand},
-    '/': function(firstOperand, secondOperand) {return firstOperand / secondOperand},
+    '/': function(firstOperand, secondOperand) {
+        if (secondOperand === 0) {return "Universe Imploding..."}
+        return firstOperand / secondOperand},
 };
 
 // console.log(compute['/'](2,3));
 
 let screenText = document.getElementsByClassName('screen')[0]
 // console.log(screenText)
-screenText.textContent = startingScreen
+// screenText.textContent = startingScreen
 
 const numbersList = [...document.querySelectorAll('.number')]
 // console.log(numbersList)
 
 numbersList.forEach(element => {
     element.addEventListener('click', e => {
-        if (!firstOperand) {
-            firstOperand = element.innerText;
+        if (!focusOnSecondOperand) {
+            firstOperand += element.innerText;
             screenText.textContent = firstOperand
             return
         } ;
-        if (!secondOperand) {
-            secondOperand = element.innerText;
+        if (focusOnSecondOperand) {
+            secondOperand += element.innerText;
             screenText.textContent = secondOperand
             return;
         };
@@ -38,8 +43,33 @@ const operatorsList = [...document.querySelectorAll('.operator')]
 
 operatorsList.forEach(element => {
     element.addEventListener('click', e => {
+        focusOnSecondOperand = 1;
         operator = element.getAttribute('action');
         screenText.textContent = element.textContent;
     })
 })
 
+const equalsButton = document.querySelector('.equals')
+equalsButton.addEventListener('click', e => {
+    // result = parseFloat((compute[operator](Number(firstOperand),Number(secondOperand))).toFixed(9));
+    result = ((compute[operator](Number(firstOperand),Number(secondOperand))).toPrecision(9));
+    firstOperand = result;
+    secondOperand = '';
+    screenText.textContent = result;
+})
+
+const clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', e => {
+    screenText.textContent = '';
+    firstOperand = '' ;
+    secondOperand = '' ;
+    operator = '';
+    focusOnSecondOperand = '';
+})
+
+const deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', e => {
+    let trimmedScreenText = screenText.innerText.slice(0, -1)
+    screenText.innerText = trimmedScreenText;
+    secondOperand = secondOperand.slice(0, -1);
+})
